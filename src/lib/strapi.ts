76f93393d131d42af7f817a23d9f1d83e8e54ca5
Developer <bilalll9889@gmail.com>
2025-08-23 12:31:55 +0500

    strapi setup

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -1,6 +1,6 @@
 import { BlogPost, BlogResponse } from "@/types/blog";
 const STRAPI_URL = process.env.STRAPI_URL || 'https://api.tfbusinesssolutions.com';
-const API_TOKEN = process.env.STRAPI_API_TOKEN;
+const API_TOKEN = process.env.STRAPI_API_TOKEN || 'b406b382ca1329ac35c9b0df167b8bc6ddf7cc2d2b6d4480124e105aaa1a8abe29389ffe02a84eb5705eeee4d6c87a130b9880fa199e1e55e4391c1838aaf9140f607fae69ad667546f4bc20b898f24f8176bf602833a13b5b156857a8eb3b1ab7e5599a7323e616f5bb6c0f25dd278ba9597866e5558b194ac1e37f0a526ca6';
 if (!API_TOKEN) {
     console.warn('⚠️ STRAPI_API_TOKEN is not set - API calls may fail');
 }
