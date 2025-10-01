2d1750773b6c172edfcd4e3f67b57feed508a43a
Developer <bilalll9889@gmail.com>
2025-10-01 18:36:36 +0500

    remove sentry for now

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,13 +1,8 @@
-import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
 
 const CustomErrorComponent = (props:any) => {
   return <Error statusCode={props.statusCode} />;
 };
 
-CustomErrorComponent.getInitialProps = async (contextData:any) => {
-  await Sentry.captureUnderscoreErrorException(contextData);
-  return Error.getInitialProps(contextData);
-};
 
 export default CustomErrorComponent;
